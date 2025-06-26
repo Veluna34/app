253f5fd22a193208dd7340f6b5a1e534d515c770
aidenf-prog <aidenf752@gmail.com>
2025-06-25 23:11:28 -0400

    fixed port issues

--- app.py
+++ app.py
@@ -1962,8 +1962,10 @@ def originalgames():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
 
 
